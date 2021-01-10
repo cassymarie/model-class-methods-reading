31155befe8d5c42ad4f00e3025d3faafca07233e
cassymarie <cassy2205@gmail.com>
2021-01-10 13:19:06 -0700

    commit tree 2a3bae89601b7b1a5e6e21d64ebc450ab1a6a8ab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,29 @@
 class PostsController < ApplicationController
-
+  #helper_method :params #Reading params is a controller concern, so we don't need to expose it to the views
+  
   def index
-    @posts = Post.all
+  # provide a list of authors to the view for the filter control
+  @authors = Author.all
+  
+  #Step 3
+  # separation of concerns, the controller should NOT have deep knowledge of the database and make direct queries.
+  # filter the @posts list based on user input
+    if !params[:author].blank?
+      #Step3:  @posts = Post.where(author: params[:author])
+      @posts = Post.by_author(params[:author]) #use a helper instead
+
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        #Step3:  @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+        @posts = Post.from_today
+      else
+        #Step3:  @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+        @posts = Post.old_news
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
   end
 
   def show
